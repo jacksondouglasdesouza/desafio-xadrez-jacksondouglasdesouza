786417861fd781cdf6f808e34a46ba120819d3e1
Jakson Douglas <140694536+jacksondouglasdesouza@users.noreply.github.com>
2026-05-20 00:01:07 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -14,6 +14,15 @@ int main()
    char direcaoBispo[] = "Cima Direita";
    char direcaoRainha[] = "Esquerda";
 
+   // Cavalo
+
+   int totalMovimentosL = 1;
+   int casasBaixoCavalo = 2;
+   int casasEsquerdaCavalo = 1;
+
+   char direcaoCavaloBaixo[] = "Baixo";
+   char direcaoCavaloEsquerda[] = "Esquerda";
+
    printf("--- Simulacão de Movimento: TORRE ---\n");
 
    for (int i = 0; i < movimentosTorre; i++)
@@ -40,6 +49,28 @@ int main()
       printf("%s\n", direcaoRainha);
       contadorRainha++;
    } while (contadorRainha < movimentosRainha);
+
+   // SIMULAÇÃO DO CAVALO
+
+   printf("\n--- Simulacão de Movimento: CAVALO ---\n");
+
+   for (int i = 0; i < totalMovimentosL; i++)
+   {
+
+      int j = 0;
+      while (j < casasBaixoCavalo)
+      {
+         printf("%s\n", direcaoCavaloBaixo);
+         j++;
+      }
+
+      int k = 0;
+      do
+      {
+         printf("%s\n", direcaoCavaloEsquerda);
+         k++;
+      } while (k < casasEsquerdaCavalo);
+   }
 
    return 0;
 }
